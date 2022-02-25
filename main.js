26dd93f97d2e4a719d85090f01408fdb2f0829f6
Steven Kitzes <steven.kitzes@gmail.com>
2022-02-25 01:56:37 -0800

    feat: add initial js and styling

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 console.log('Hello, there!');
+
+const guitar = document.getElementById('guitar');
+
+const bridge = document.getElementById('bridge');
+const neck = document.getElementById('neck');
+
+const clean = document.getElementById('clean');
+const distorted = document.getElementById('distorted');
+
+const name = document.getElementById('name');
+const details = document.getElementById('details');
+
+const imageContainer = document.getElementById('image-container');
+
+bridge.checked = true;
+clean.checked = true;
+
+guitar.addEventListener('change', update);
+bridge.addEventListener('change', update);
+neck.addEventListener('change', update);
+clean.addEventListener('change', update);
+distorted.addEventListener('change', update);
+
+function update() {
+  if (guitar.value === 'placeholder') {
+    name.style.backgroundColor = '#FFAAAA';
+    setTimeout(() => {
+      name.style.backgroundColor = 'unset';
+    }, 1000)
+    return;
+  }
+}
